2f883b2b2561fe87aec0cbbbc31f4dc8361dd14d
psso2004 <psso2004@gmail.com>
2016-08-18 23:31:55 +0900

    Add loadFile to index.js and main/writer.hmtl But not working..

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
 const http = require('http')
+const fs = require('fs')
+const characterSet = 'utf-8'
+const appDir = 'app'
+const viewDir = appDir + '/view'
 
 const hostname = '127.0.0.1'
 const port = 3000
 
-const requestHandler = (request, response) => {
-  var response_template = (content) => {
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'text/plain')
-    response.end(content)
-  }
+const responseTemplate = (content, statusCode, contentType) => {
+  response.statusCode = statusCode
+  response.setHeader('Content-Type', contentType)
+  response.end(content)
+}
 
+const loadFile = (path) => {
+  fs.readFile(path, characterSet, (err, data) => {
+	if (err) {
+	  return ['', err] 
+	}
+
+	[data, '']
+  })
+}
+
+const requestHandler = (request, response) => {
   if (request.url == '/') {
-    response_template('Root!')
+    responseTemplate('Root!', 200, 'text/plain')
   }
 
   if (request.url == '/board') {
-    switch (request.method) {
-    case 'GET':
-      response_template('Board Show!')
-      break
-    case 'POST':
-      response_template('Board Create!')
-      break
-    case 'PUT':
-      response_template('Board Update!')
-      break;
-    case 'DELETE':
-      response_template('Board Delete!')
-      break;
-    default:
-      response_template('Board Index!')
-    }
+	let file = loadFile(`${viewDir}/main/writer.html`)
+	
+	if (file[1] == '') {
+	  responseTemplate(file[0], 200, 'text/html')
+	} else {
+	  responseTemplate(file[1], 500, 'text/plain')
+	}
   }
 }
 
